eb5e389d5389913cfc2c0ac2b2264fee0fe048a1
Rafalstrus <76953304+Rafalstrus@users.noreply.github.com>
2021-09-10 10:45:16 +0200

    add new fetches, root global styles, changes in calendar

--- src/components/calendar/change-month.component.tsx
+++ src/components/calendar/change-month.component.tsx
@@ -1,19 +1,41 @@
 import {Box,Button} from '@material-ui/core/';
 
-export const ChangeMonthPanel =() =>(
+
+import { mapStateToProps, mapDispatchToProps } from './redux-store/operations';
+import { connect, useSelector } from "react-redux";
+
+import {getMonthData} from '../../fetches'
+
+const ChangeMonthPanel =({setDate,setMonthData} :any) =>{
+     const date = useSelector((state :any) => state.date)
+     return(
 <Box id="change-month-panel">
 <Button
 variant="contained"
 onClick={
-    ()=>{}
+    async ()=>{
+        setDate(date.setMonth(date.getMonth() -1))
+        let data = await getMonthData(date.getMonth()+1)
+        setMonthData(data)
+    }
 }
-></Button>
+>{'<<<'}</Button>
 <p>{date.getMonth()+1+'/'+date.getFullYear()}</p>
 <Button
 variant="contained"
 onClick={
-    ()=>{}
+    async ()=>{
+        setDate(date.setMonth(date.getMonth() +1))
+        let data = await getMonthData(date.getMonth()+1)
+        setMonthData(data)
+    }
 }
-></Button>
+>{'>>>'}</Button>
 </Box>
-    )
+     )
+ }
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(ChangeMonthPanel);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './root.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import store from "./store-redux/store";
